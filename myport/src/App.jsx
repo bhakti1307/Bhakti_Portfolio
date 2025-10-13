90a0b38837fed0d02a14f9c91b058fd416895e60
Bhakti joshi <87695740+bhakti1307@users.noreply.github.com>
2025-10-13 23:58:23 +0530

    Update App.jsx

--- myport/src/App.jsx
+++ myport/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Router, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
