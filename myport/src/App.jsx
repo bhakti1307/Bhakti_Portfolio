539bcfbe981f2486ec57781a7eabc3cc21d7610a
Bhakti joshi <87695740+bhakti1307@users.noreply.github.com>
2025-10-13 23:57:42 +0530

    Update App.jsx

--- myport/src/App.jsx
+++ myport/src/App.jsx
@@ -12,15 +12,26 @@ function AnimatedRoutes() {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </AnimatePresence>
+    <Router>
+      <div className="min-h-screen bg-[#0A192F] text-white relative overflow-x-hidden">
+        {/* Particle Background */}
+        <ParticleBackground />
+
+        {/* Navigation */}
+        <Navigation />
+
+        {/* Main Content */}
+        <main className="relative z-10">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
